3622dc87e4225d15506e8a3f809882c3e1ed90bb
ZuuZaa <zumbaghirova@gmail.com>
2024-04-09 22:29:59 +0400

    fix: fix destination moon image path

--- src/config/constans.js
+++ src/config/constans.js
@@ -29,7 +29,7 @@ export const NAV_LINKS = {
     items: [
       {
         name: "moon",
-        image: "../../public/images/destination/image-moon.png",
+        image: "images/destination/image-moon.png",
         description:
           "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
         distance: "384,400",
